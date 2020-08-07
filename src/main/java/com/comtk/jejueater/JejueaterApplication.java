148cf838a6dca5e3972d2da889ed315a7e7fe09d
김주석 <kjs20305@gmail.com>
2020-08-08 01:14:02 +0900

    init

---
+++ src/main/java/com/comtk/jejueater/JejueaterApplication.java
@@ -0,0 +1,13 @@
+package com.comtk.jejueater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejueaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejueaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comtk/jejueater/JejueaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comtk.jejueater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejueaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
